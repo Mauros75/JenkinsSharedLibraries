56743a334ede20d3f8fc04d496b1c6d3a94d12a4
Mauros75 <105608358+Mauros75@users.noreply.github.com>
2022-07-18 14:49:25 -0600

    Create sharedFile.groovy

---
+++ sharedFile.groovy
@@ -0,0 +1,21 @@
+def call(String stageName) {
+    if ("${stageName}" == 'Build') {
+        sh 'mvn clean install'
+    }
+    else if ("${stageName}" == 'CodeQuality') {
+        sh 'mvn sonar:sonar'
+    }
+    else if ("${stageName}" == 'BackupArtifacts') {
+        sh 'mvn deploy'
+        sh 'echo Running code Quality analysis'
+    }
+    else if ("${stageName}" == 'Authorisation') {
+      timeout(time: 24, unit: 'HOURS') {
+    // some block
+    input message: 'Approve or decline'
+}
+    else if ("${stageName}" == 'deploy') {
+        sh 'echo app now ready for deployment'
+        sh 'scp -i *.war ec2-user@172.10.2:/opt/tomcta9/webapps/'
+    }
+}
